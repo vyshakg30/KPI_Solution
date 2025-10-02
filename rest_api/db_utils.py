0bcfe0ac48ac144a32aa3241be88e67df8264e3e
vyshakg30 <5340250+vyshakg30@users.noreply.github.com>
2025-10-03 00:02:13 +0530

    move sql statement function to db_util

--- rest_api/db_utils.py
+++ rest_api/db_utils.py
@@ -21,3 +21,11 @@ def query_as_dict(sql: str, params=None):
             cur = con.execute(sql, params or [])
             cols = [c[0] for c in cur.description]
             return [dict(zip(cols, row)) for row in cur.fetchall()]
+
+
+def get_alerts_from_db(date_from: str, date_to: str):
+    return query_as_dict(f"select * FROM dev.gold.gold_alerts_daily where alert_date between '{date_from}' and '{date_to}'")
+
+def get_kpi_from_db(date_from: str, date_to: str):
+    return query_as_dict(f"select * FROM dev.gold.gold_daily_kpi where funded_at between '{date_from}' and '{date_to}'")
+
--- rest_api/main.py
+++ rest_api/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
-from db_utils import query_as_dict, config
+
+from db_utils import query_as_dict, config, get_kpi_from_db, get_alerts_from_db
 
 app = FastAPI(title=config["app"]["name"])
+
 
 #  Global Exception Handler
 @app.exception_handler(Exception)
@@ -17,30 +19,35 @@ async def global_exception_handler(request: Request, exc: Exception):
     )
 
 
-
 # API Endpoints
-@app.get("/api/kpisall")
-def get_kpis():
-    return {"kpis": query_as_dict("SELECT * FROM dev.gold.gold_daily_kpi limit 100000")}
 
 @app.get("/api/kpis")
 def get_kpis(date: str):
-    return {"date": date, "kpis": query_as_dict(f"SELECT * FROM dev.gold.gold_daily_kpi where funded_at = '{date}'")}
+    return {
+        "date": date,
+        "kpis": get_kpi_from_db(date, date)
+    }
+
 
 @app.get("/api/alerts")
 def get_alerts(date_from: str, date_to: str):
     return {
         "from": date_from,
         "to": date_to,
-        "alerts": query_as_dict(f"SELECT * FROM dev.gold.gold_alerts_daily where alert_date between '{date_from}' AND '{date_to}'")
+        "alerts": get_alerts_from_db(date_from, date_to)
     }
+
 
 @app.get("/api/report")
 def get_report(date_from: str, date_to: str):
-
-
     return {
         "from": date_from,
         "to": date_to,
-        "reports": query_as_dict("SELECT * FROM reports WHERE date BETWEEN ? AND ? ORDER BY date", [date_from, date_to])
+        "kpi": get_kpi_from_db(date_from, date_to),
+        "alerts": get_alerts_from_db(date_from, date_to)
     }
+
+
+@app.get("/api/kpisall")
+def get_kpis():
+    return {"kpis": query_as_dict("SELECT * FROM dev.gold.gold_daily_kpi limit 100000")}
